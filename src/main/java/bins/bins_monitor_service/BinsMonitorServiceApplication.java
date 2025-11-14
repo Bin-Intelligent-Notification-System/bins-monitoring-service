6c32a46cd4b366a8cceffa1727acd51109093769
Gianluca Girardi <gl.girardi1@gmail.com>
2025-11-14 12:13:07 -0500

    first commit

---
+++ src/main/java/bins/bins_monitor_service/BinsMonitorServiceApplication.java
@@ -0,0 +1,13 @@
+package bins.bins_monitor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinsMonitorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinsMonitorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bins/bins_monitor_service/BinsMonitorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bins.bins_monitor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinsMonitorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
